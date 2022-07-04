c32ca5f93afd3e552bd1d4d363fdbdfb736dc580
MshSiam <siyamkenway@gmail.com>
2022-07-04 09:32:28 +0600

    app.route useing

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ app.get("/", (req, res) => {
   res.send("welcome")
 })
 
+//  param //
+app.param("id", (req, res, next, id) => {
+  const user = {
+    userId: id,
+    name: "siam"
+  }
+  req.userDetails = user
+
+  next()
+})
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.userDetails)
+  res.send("welcome to application home.")
+})
+
+// //////
+
+// app.route
+
+app.post("/about/mission", (req, res) => {
+  res.send("welcome to post")
+})
+
+app.put("/about/mission", (req, res) => {
+  res.send("welcome to put")
+})
+
+// instead of this, we can use app.route to avoid duplicate code and typo errors.
+app
+  .route("/about/mission")
+  .get((req, res) => {
+    res.send("welcome to about mission GET")
+  })
+  .post((req, res) => {
+    res.send("welcome to about mission POST")
+  })
+  .put((req, res) => {
+    res.send("welcome to about mission PUT")
+  })
+
+// ////////
 app.use("/admin", admin)
 
 app.listen(3000, () => {
