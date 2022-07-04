b5a0b407cf0ffef328bcbcf023c1f381695cc742
MshSiam <siyamkenway@gmail.com>
2022-07-04 09:49:49 +0600

    template engine render

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const admin = express()
 
+app.set("view engine", "ejs")
+
 admin.get("/dashboard", (req, res) => {
   console.log(admin.mountpath)
   res.send("welcome to dashboard")
@@ -31,29 +33,42 @@ app.get("/user/:id", (req, res) => {
 
 // app.route
 
-app.post("/about/mission", (req, res) => {
-  res.send("welcome to post")
-})
+// app.post("/about/mission", (req, res) => {
+//   res.send("welcome to post")
+// })
 
-app.put("/about/mission", (req, res) => {
-  res.send("welcome to put")
-})
+// app.put("/about/mission", (req, res) => {
+//   res.send("welcome to put")
+// })
 
 // instead of this, we can use app.route to avoid duplicate code and typo errors.
+// app
+//   .route("/about/mission")
+//   .get((req, res) => {
+//     res.send("welcome to about mission GET")
+//   })
+//   .post((req, res) => {
+//     res.send("welcome to about mission POST")
+//   })
+//   .put((req, res) => {
+//     res.send("welcome to about mission PUT")
+//   })
+
+// ////////
+app.use("/admin", admin)
+
+///////////////// ==================== template engine ==================== /////////////
 app
   .route("/about/mission")
   .get((req, res) => {
-    res.send("welcome to about mission GET")
+    res.render("index")
   })
   .post((req, res) => {
-    res.send("welcome to about mission POST")
+    res.render("pages/about")
   })
   .put((req, res) => {
     res.send("welcome to about mission PUT")
   })
-
-// ////////
-app.use("/admin", admin)
 
 app.listen(3000, () => {
   console.log("running")
