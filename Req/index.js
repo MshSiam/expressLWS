c46477017975e7f98911894d0a542def41053956
MshSiam <siyamkenway@gmail.com>
2022-07-04 12:28:24 +0600

    url

--- Req/index.js
+++ Req/index.js
@@ -5,12 +5,18 @@ const adminRoute = express.Router()
 app.use("/admin", adminRoute)
 
 adminRoute.get("/dashboard", (req, res) => {
-  console.log(req.baseUrl)
+  //   console.log(req.baseUrl)
+  console.log(req.originalUrl)
+  console.log(req.url)
   res.send("we are in admin dashboard")
 })
 
 app.get("/user/:id", (req, res) => {
-  console.log(req.baseUrl)
+  //   console.log(req.baseUrl)
+
+  console.log(req.originalUrl)
+  console.log(req.url)
+
   res.send("welcome")
 })
 
